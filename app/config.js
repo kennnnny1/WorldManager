4bbff6a24d1098a222a5f579aaaa5ed9479471f6
Ryan Ostrager <ryan.ostrager@gmail.com>
2014-01-08 15:53:07 +0000

    Fixed minor edit profile button text. Updated copyright to 2014.

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-"url" : "http://rugrid.rutgers.edu",
-"port" : 3003,
+"url" : "http://worldmanager.rutgers.edu",
+"port" : 3000,
 "db" : "WorldManagerRyan",
 "opentokapi" : "24960472",
 "opentoksecret" : "e7bbe63fba07b4c6939f2059eea24e4933667a63"
