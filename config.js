3560dc1ef9b8d2e6dbf976a5c05e8111fdb64888
Ryan Ostrager <ryan.ostrager@gmail.com>
2014-03-04 16:12:11 +0000

    Added files that were moved in previous commit.

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'url' : 'http://worldmanager.rutgers.edu',
+	'port' : 3001,
+	'db' : 'WorldManagerRyan',
+	'opentokapi' : '10120602',
+	'opentoksecret' : '41c132f9c35f3278f4d2b78b8f87bb4b94210b3d'
+};
---
+++ static/js/opentok2opentok.js
@@ -0,0 +1,48 @@
+//Vivox functions that could be called by or have been called from unity web player
+var prevRoom = '';
+var session;
+var world = '001';
+function initUser(player) {
+  console.log('intiUser: Start');
+  console.log(sessions);
+  user = player;
+  sessions[room].connect(apikey, tokens[room]);
+  $('#rooms').text('Room: ' + room);
+  //set publishing options
+  var pubOptions = {
+    publishAudio: config.mic,
+    publishVideo: config.video,
+    height: 1,
+    width: 1,
+    name: user
+  };
+  publisher = TB.initPublisher(apikey, 'publisherContainer', pubOptions);
+  console.log('OpenTOK url: ' + '/api/' + '001/' + user + '/' + currentRoom);
+  console.log('initUser End: ' + player);
+}
+
+function RoomChange(newRoom) {
+  //This is where the vivox session disconnect and connect code should go.
+  console.log('RoomChange: ' + newRoom);
+  //If room acutally changes switch
+  if (newRoom != currentRoom) {
+    if (currentRoom != undefined) {
+	unpublish(currentRoom);
+	sessions[currentRoom].disconnect();
+    }
+    currentRoom = newRoom;
+    sessions[newRoom].connect(apikey, tokens[newRoom]);
+    $('#rooms').text('Room: ' + currentRoom);
+  }
+  console.log('RoomChange End');
+}
+//mute myself
+function MicMute(mute) {
+  console.log('MicMute: ' + mute);
+  toggleAudio(!mute);
+}
+//mute other people
+function SetVolume(newVolume) {
+  console.log('SetVolume: ' + isMuted);
+  $('#video').volume = newVolume;
+}
