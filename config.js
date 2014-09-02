ef17a3e61086047de04aba574afa04f23ccd48eb
Ryan Ostrager <ryan.ostrager@gmail.com>
2014-09-02 15:23:46 +0000

    Added in missing img files.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'url' : 'http://worldmanager.rutgers.edu',
-	'port' : 3001,
+	'port' : 3000,
 	'db' : 'WorldManagerRyan',
 	'opentokapi' : '10120602',
 	'opentoksecret' : '41c132f9c35f3278f4d2b78b8f87bb4b94210b3d'
